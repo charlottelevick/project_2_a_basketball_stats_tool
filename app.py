0e7f72b3c5d954a36e357987fc40f019da097f63
charlottelevick <lottielevick_@hotmail.co.uk>
2025-01-05 14:49:13 +0000

    Created and clean_data function
    Height as integer
    Experience as boolean value
    Guardian split

--- app.py
+++ app.py
@@ -1,2 +1,27 @@
 from constants import TEAMS
 from constants import PLAYERS
+
+def clean_data(data):
+    cleaned = []
+    for player in data:
+        fixed = {}
+        fixed["name"] = player["name"]
+        
+        guardians_list = []
+        if "and" in player["guardians"]:
+            guardians_list = player["guardians"].split(" and ")
+        else:
+            guardians_list.append(player["guardians"]) 
+        fixed["guardians"] = guardians_list
+        
+        split_height_as_str = player["height"].split(" ")[0]
+        fixed["height"] = int(split_height_as_str)
+        
+        if player["experience"] == "YES":
+            fixed["experience"] = True
+        else:
+            fixed["experience"] = False
+        cleaned.append(fixed)
+    return cleaned
+    
+print(clean_data(PLAYERS))
