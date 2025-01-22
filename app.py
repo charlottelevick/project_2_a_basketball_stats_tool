66bbab17802913a6f04631431eeadcbe5a80e485
charlottelevick <lottielevick_@hotmail.co.uk>
2025-01-22 20:06:25 +0000

    Adding MENU

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from constants import TEAMS, PLAYERS
+import sys
 
 def clean_data(player_list):
     cleaned = []
@@ -20,20 +21,21 @@ def clean_data(player_list):
         cleaned.append(fixed)
     return cleaned
 
-    
 n = len(PLAYERS)
 split_size = n // 3
 team1 = PLAYERS[0:split_size]
 team2 = PLAYERS[split_size:2*split_size]
 team3 = PLAYERS[2*split_size:]
-print(f'Panthers: {team1}\n')
-print(f'Bandits : {team2}\n')
-print(f'Warriors : {team3}\n')
+#print(f'Panthers: {team1}\n')
+#print(f'Bandits : {team2}\n')
+#print(f'Warriors : {team3}\n')
 
-#def balance_teams(player_list, teams):
-    #NUM_PLAYERS_TEAM = len(player_list) / len(teams)
-    #print(int(NUM_PLAYERS_TEAM))
-    
-#balance_teams = []
-    
-#print(clean_data(PLAYERS))
+user_choice = input("BASKETBALL TEAM STATS TOOL\n\n----MENU----\n\nHere are your choices:\nA) Display Team Stats\nB) Quit\n")
+if user_choice.upper() == "A":
+    user_option = input("Enter an option:\nA) Panthers\nB) Bandits\nC) Warriors\n")
+    if user_option.upper() == "A":
+        print(f"Team: Panther's Stats\n--------------\nTotal players: {}\nTotal experienced: ?\nTotal inexperienced: \nAverage height: ?\nPlayers on Team: ?\nGuardians: ?\nPress ENTER to continue...")
+elif user_choice.upper() == "B":
+    sys.exit()
+else:
+    print("Please try again")
