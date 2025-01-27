710e1a2d8bf2569a0f3c67b6b7a7540b6dc22e64
charlottelevick <lottielevick_@hotmail.co.uk>
2025-01-27 17:23:44 +0000

    Average height, experienced and inexperienced functions

--- app.py
+++ app.py
@@ -21,31 +21,63 @@ def clean_data(player_list):
         cleaned.append(fixed)
     return cleaned
 
-n = len(PLAYERS)
-split_size = n // len(TEAMS)
-team1 = PLAYERS[0:split_size]
-team2 = PLAYERS[split_size:2*split_size]
-team3 = PLAYERS[2*split_size:]
+cleaned_data = clean_data(PLAYERS)
 
-def start_game():
+n = len(cleaned_data)
+split_size = n // len(TEAMS)
+team1 = cleaned_data[0:split_size]
+team2 = cleaned_data[split_size:2*split_size]
+team3 = cleaned_data[2*split_size:]
+
+def get_experienced_players(team):
+    experienced_players = []
+    for player in team:
+        if player["experience"] == True:
+            experienced_players.append(player)
+    return experienced_players
+    
+team1_experienced_players = get_experienced_players(team1)
+team2_experienced_players = get_experienced_players(team2)
+team3_experienced_players = get_experienced_players(team3)
+
+def get_inexperienced_players(team):
+    inexperienced_players = []
+    for player in team:
+        if player["experience"] == False:
+            inexperienced_players.append(player)
+    return inexperienced_players
+
+team1_inexperienced_players = get_inexperienced_players(team1)
+team2_inexperienced_players = get_inexperienced_players(team2)
+team3_inexperienced_players = get_inexperienced_players(team3)
+
+def average_player_heights(team):
+    player_heights = []
+    for player in team:
+        height = player["height"] 
+        player_heights.append(height)  
+    average_height = sum(player_heights) / len(player_heights)
+    return average_height
+
+def start_stats_tool():
     user_choice = input("BASKETBALL TEAM STATS TOOL\n\n----MENU----\n\nHere are your choices:\nA) Display Team Stats\nB) Quit\n")
     if user_choice.upper() == "A":
         user_option = input("\nEnter an option:\nA) Panthers\nB) Bandits\nC) Warriors\n")
         if user_option.upper() == "A":
-            print(f"\nTeam: Panther's Stats\n--------------\nTotal players: {len(team1)}\nTotal experienced: ?\nTotal inexperienced: \nAverage height: ?\nPlayers on Team: ?\nGuardians: ?")
+            print(f"\nTeam: Panther's Stats\n-----------------\nTotal players: {len(team1)}\nTotal experienced: {len(team1_experienced_players)}\nTotal inexperienced: {len(team1_inexperienced_players)}\nAverage height: {average_player_heights(team1)}\nPlayers on Team: ?\nGuardians: ?")
             input("\nPress ENTER to continue...\n")
-            start_game()
+            start_stats_tool()
         elif user_option.upper() == "B":
-            print(f"\nTeam: Bandit's Stats\n--------------\nTotal players: {len(team2)}\nTotal experienced: ?\nTotal inexperienced: \nAverage height: ?\nPlayers on Team: ?\nGuardians: ?")
+            print(f"\nTeam: Bandit's Stats\n------------------\nTotal players: {len(team2)}\nTotal experienced: {len(team2_experienced_players)}\nTotal inexperienced: {len(team2_inexperienced_players)}\nAverage height: {average_player_heights(team2)}\nPlayers on Team: ?\nGuardians: ?")
             input("\nPress ENTER to continue...\n")
-            start_game()
+            start_stats_tool()
         elif user_option.upper() == "C":
-            print(f"\nTeam: Warrior's Stats\n--------------\nTotal players: {len(team3)}\nTotal experienced: ?\nTotal inexperienced: \nAverage height: ?\nPlayers on Team: ?\nGuardians: ?")
+            print(f"\nTeam: Warrior's Stats\n-----------------\nTotal players: {len(team3)}\nTotal experienced: {len(team3_experienced_players)}\nTotal inexperienced: {len(team3_inexperienced_players)}\nAverage height: {average_player_heights(team3)}\nPlayers on Team: ?\nGuardians: ?")
             input("\nPress ENTER to continue...\n")
-            start_game()
+            start_stats_tool()
     elif user_choice.upper() == "B":
         sys.exit()
     else:
         print("Please try again")
 
-start_game()
+start_stats_tool()
